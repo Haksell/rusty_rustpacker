2f26c8705811814c8dff7f68fb8e303c67251ddf
Axel Brisse <axel.brisse@gmail.com>
2024-09-23 23:39:44 +0200

    ndisasm + segfaulting jmp

--- elk/src/main.rs
+++ elk/src/main.rs
@@ -1,4 +1,11 @@
-use std::{env, error::Error, fs, process::Command};
+use std::{
+    cmp::min,
+    env,
+    error::Error,
+    fs,
+    io::Write,
+    process::{Command, Stdio},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_path = env::args().nth(1).expect("usage: elk <file>");
@@ -10,11 +17,39 @@ fn main() -> Result<(), Box<dyn Error>> {
         delf::File::parse_or_print_error(&input[..]).unwrap_or_else(|| std::process::exit(1));
     println!("{:#?}", file);
 
-    println!("Executing {:?}...", input_path);
-    let status = Command::new(input_path).status()?;
+    println!("\nExecuting {:?}...", input_path);
+    let status = Command::new(&input_path).status()?;
     if !status.success() {
         return Err("process did not exit successfully".into());
     }
 
+    println!("\nDisassembling {:?}...", input_path);
+    let code = &input[0x1000..min(0x1025, input.len())];
+    ndisasm(code)?;
+
+    println!("\nExecuting {:?} in memory...", input_path);
+    let entry_point = code.as_ptr();
+    println!("Entry point: {:?}", entry_point);
+    unsafe { jmp(entry_point) };
+
     Ok(())
+}
+
+fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
+    let mut child = Command::new("ndisasm")
+        .arg("-b")
+        .arg("64")
+        .arg("-")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    child.stdin.as_mut().unwrap().write_all(code)?;
+    let output = child.wait_with_output()?;
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    Ok(())
+}
+
+unsafe fn jmp(addr: *const u8) {
+    let fn_ptr: fn() = std::mem::transmute(addr);
+    fn_ptr();
 }
